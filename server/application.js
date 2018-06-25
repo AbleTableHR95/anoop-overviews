7453e005dc5bc6cdb296385f620c3bc4d6e0bda0
Jehwa Shin <s.jehwa@gmail.com>
2018-06-25 16:10:56 -0700

    connected cassandra db with endpoints

--- server/application.js
+++ server/application.js
@@ -1,44 +1,60 @@
 const express = require('express');
 const path = require('path');
-const db = require('../database/index');
-const dbC = require('../database/cassandra.js');
+// const db = require('../database/index');
+const db = require('../database/cassandra.js');
 
 const app = express();
+
+let restaurantIdCount = 10000000;
 
 app.use('/restaurant/:restaurantId', express.static(path.join(__dirname, '../public/index.html')));
 app.use('/overviewsBundle.js', express.static(path.join(__dirname, '../public/dist/bundle.js')));
 app.use('/images/star-rating.png', express.static(path.join(__dirname, '../public/images/star-rating.png')));
 
 app.get('/overviews/restaurant/:restaurantId/overview', (req, res) => {
-  db.retrieve(req.params.restaurantId, (err, results) => {
-    if (err && err.message.includes('Cast to number failed for value "NaN"')) {
-      res.status(400).json('Bad request');
-    } else if (err) {
-      res.status(500).json('Unable to retrieve overview data from database');
-    } else {
-      res.status(200).json(results);
-    }
-  });
+  let id = req.url.split('/')[3];
+  db.find(id).then(data => res.send(data.rows)).catch(err => res.send(err));
+
+  // original code from anoop!!
+  // db.retrieve(req.params.restaurantId, (err, results) => {
+  //   if (err && err.message.includes('Cast to number failed for value "NaN"')) {
+  //     res.status(400).json('Bad request');
+  //   } else if (err) {
+  //     res.status(500).json('Unable to retrieve overview data from database');
+  //   } else {
+  //     res.status(200).json(results);
+  //   }
+  // });
 });
 
-app.post('/overviews/restaurant/:restaurantId/add', (req, res) => {
-  const id = req.url.split('/')[3];
-  const tag = 'Good for a date';
-  const count = 25;
-  console.log(dbC.add());
+app.post('/overviews/restaurant/:restaurantId/overview', (req, res) => {
+  db.add(restaurantIdCount + 1)
+    .then((data) => {
+      restaurantIdCount += 1;
+      console.log(restaurantIdCount, 'added id');
+      res.send(data);
+    });
+  // res.send('response for post request');
+});
 
-  dbC.add(id, tag, count)
-    .then(data => res.send(data.rows))
+app.put('/overviews/restaurant/:restaurantId/overview', (req, res) => {
+  let id = req.url.split('/')[3];
+  let tag = 'Good for a Date';
+  let count = 25;
+
+  db.change(id, tag, count)
+    .then(data => res.send(data))
     .catch(err => res.send(err));
-  // res.send(id);
 });
 
-app.put('/overviews/restaurant/:restaurantId/change', (req, res) => {
-  res.send('response for put request');
-});
-
-app.delete('/overviews/restaurant/:restaurantId/delete', (req, res) => {
-  res.send('response for delete request');
+app.delete('/overviews/restaurant/:restaurantId/overview', (req, res) => {
+  db.del(restaurantIdCount)
+    .then((data) => {
+      console.log(restaurantIdCount, 'deleted id');
+      restaurantIdCount --; 
+      res.send(data);
+    })
+    .catch(err => res.send(err));
 });
 
 module.exports = app;
